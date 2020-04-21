32d7f639585a90f026098694fb13492ec4f2bdb3
Michael Santos <michael.joseph.santos@gmail.com>
2020-04-21 12:25:28 -0400

    LeetCode: moved problems to separate classes

---
+++ Testbed/GroupAnagrams.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Testbed
+{
+    public class GroupAnagrams
+    {
+        public IList<IList<string>> Run(string[] strs)
+        {
+            var strDict = new Dictionary<string, IList<string>>();
+
+            foreach (var str in strs)
+            {
+                var sortedStr = String.Concat(str.OrderBy(c => c).ToList());
+
+                if (strDict.ContainsKey(sortedStr))
+                {
+                    strDict[sortedStr].Add(str);
+                }
+                else
+                {
+                    strDict[sortedStr] = new List<string> { str };
+                }
+            }
+
+            var retVal = strDict.Values.ToList();
+            return (retVal as IList<IList<string>>);
+        }
+    }
+}
---
+++ Testbed/MoveZeroes.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Testbed
+{
+    public class MoveZeroes
+    {
+        public void Run(int[] nums)
+        {
+            if (nums.Length <= 1) return;
+
+            var lastIndex = nums.Length - 1;
+
+            for (var i = lastIndex - 1; i >= 0; i--)
+            {
+                Console.Out.WriteLine($"[{string.Join(",", nums)}] - loop start");
+
+                if (nums[i] == 0)
+                {
+                    for (int j = i; j < lastIndex; j++)
+                    {
+                        nums[j] = nums[j + 1];
+                    }
+
+                    nums[lastIndex] = 0;
+                    lastIndex--;
+                }
+
+                Console.Out.WriteLine($"[{string.Join(",", nums)}] - loop end");
+            }
+
+            Console.Out.WriteLine($"[{string.Join(",", nums)}] - FINAL");
+        }
+    }
+}
