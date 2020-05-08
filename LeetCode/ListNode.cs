039afc64e1cc2c68d8a4d5eb48b9c6b81baf5f6b
Michael Santos <michael.joseph.santos@gmail.com>
2020-05-08 12:03:41 -0400

    Moved ListNode to own file  +ToString()/Equals()

---
+++ LeetCode/ListNode.cs
@@ -0,0 +1,75 @@
+using System.Text;
+
+namespace LeetCode
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+
+        public static ListNode Build(int[] intArray)
+        {
+            ListNode head = null; ;
+            ListNode iterationNode = null;
+
+            foreach (var num in intArray)
+            {
+                var newNode = new ListNode(num);
+
+                if (head == null)
+                {
+                    head = newNode;
+                    iterationNode = newNode;
+                }
+                else
+                {
+                    iterationNode.next = newNode;
+                    iterationNode = newNode;
+                }
+            }
+
+            return head;
+        }
+
+        public override string ToString()
+        {
+            var node = this;
+            var strBuilder = new StringBuilder();
+
+            while (node != null)
+            {
+                strBuilder.Append(node.val);
+
+                if (node.next != null)
+                    strBuilder.Append(" > ");
+
+                node = node.next;
+            }
+
+            return strBuilder.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var comparisonNode = obj as ListNode;
+            
+            if (comparisonNode == null)
+                return false;
+
+            var thisNode = this;
+
+            while (thisNode != null)
+            {
+                if (comparisonNode == null || thisNode.val != comparisonNode.val)
+                    return false;
+
+                thisNode = thisNode.next;
+                comparisonNode = comparisonNode.next;
+            }
+
+            return comparisonNode == null;
+        }
+    }
+}
--- LeetCode/Solutions/MiddleNode.cs
+++ LeetCode/Solutions/MiddleNode.cs
@@ -1,59 +1,7 @@
 using System;
-using System.Linq;
-using System.Text;
-using System.Collections.Generic;
 
 namespace LeetCode
 {
-    // Definition for singly-linked list.
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int x) { val = x; }
-
-        public static ListNode Build(int[] intArray)
-        {
-            ListNode head = null;;
-            ListNode iterationNode = null;
-
-            foreach(var num in intArray)
-            {
-                var newNode = new ListNode(num);
-
-                if (head == null)
-                {
-                    head = newNode;
-                    iterationNode = newNode;
-                }
-                else
-                {
-                    iterationNode.next = newNode;
-                    iterationNode = newNode;
-                }
-            }
-
-            return head;
-        }
-
-        public static string Output(ListNode node)
-        {
-            var strBuilder = new StringBuilder();
-
-            while (node != null)
-            {
-                strBuilder.Append(node.val);
-
-                if (node.next != null)
-                    strBuilder.Append(" > ");
-
-                node = node.next;
-            }
-
-            return strBuilder.ToString();
-        }
-    }
-
     public partial class Solution
     {
         public ListNode MiddleNode(ListNode head)
