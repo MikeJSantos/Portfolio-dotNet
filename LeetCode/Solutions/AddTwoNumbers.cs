4bc4386b04731eb0721e15fd26434be552aa7a6b
Michael Santos <michael.joseph.santos@gmail.com>
2020-05-18 14:20:04 -0400

    AddTwoNumbers accepted

--- LeetCode/Solutions/AddTwoNumbers.cs
+++ LeetCode/Solutions/AddTwoNumbers.cs
@@ -7,46 +7,52 @@ namespace LeetCode
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            var num1 = AddTwoNumbers_GetReversedNumFromLL(l1);
-            var num2 = AddTwoNumbers_GetReversedNumFromLL(l2);
-            var addedNum = num1 + num2;
+            ListNode retVal = null,
+                        tail = null,
+                        newNode;
+            int l1val,
+                l2val,
+                sum,
+                remainder = 0;
 
-            ListNode retVal = null, currentNode = null, newNode;
-
-            while (addedNum >= 1.0)
+            while (l1 != null || l2 != null)
             {
-                var modulus = Convert.ToInt32(Math.Floor(addedNum % 10));
-                newNode = new ListNode(modulus);
+                l1val = (l1 != null) ? l1.val : 0;
+                l2val = (l2 != null) ? l2.val : 0;
+                sum = l1val + l2val + remainder;
+                remainder = 0;
 
-                if (retVal == null)
+                if (sum > 9)
                 {
-                    retVal      = newNode;
-                    currentNode = newNode;
+                    remainder = sum / 10;
+                    sum = sum % 10;
+                }
+
+                newNode = new ListNode(sum);
+                if (tail != null)
+                {
+                    tail.next = newNode;
+                    tail = newNode;
                 }
                 else
                 {
-                    currentNode.next = newNode;
-                    currentNode      = newNode;
+                    retVal = newNode;
+                    tail = newNode;
                 }
 
-                addedNum = addedNum / 10;
+                l1 = l1?.next;
+                l2 = l2?.next;
             }
 
-            return retVal ?? new ListNode(0);
-        }
-
-        private double AddTwoNumbers_GetReversedNumFromLL(ListNode root)
-        {
-            var strBuilder = new StringBuilder();
-            var node = root;
-
-            while (node != null)
+            while (remainder > 0)
             {
-                strBuilder.Insert(0, node.val);
-                node = node.next;
+                newNode   = new ListNode(remainder % 10);
+                tail.next = newNode;
+                tail      = newNode;
+                remainder = remainder / 10;
             }
 
-            return double.Parse(strBuilder.ToString());
+            return retVal;
         }
     }
 }
