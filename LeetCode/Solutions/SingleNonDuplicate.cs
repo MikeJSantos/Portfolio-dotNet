ae68ef2c38dea28f95ed8c0202d93f491b682621
Michael Santos <michael.joseph.santos@gmail.com>
2020-05-18 22:42:05 -0400

    SingleNonDuplicate moved unit test

--- LeetCode/Solutions/SingleNonDuplicate.cs
+++ LeetCode/Solutions/SingleNonDuplicate.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Collections.Generic;
+using Xunit;
 
 namespace LeetCode
 {
@@ -19,6 +20,22 @@ namespace LeetCode
             }
 
             return dict.Keys.FirstOrDefault();
+        }
+    }
+
+    public partial class UnitTests
+    {
+        [Fact]
+        public void SingleNonDuplicateTest()
+        {
+            var s = new Solution();
+            int[] nums;
+
+            nums = new int[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+            Assert.Equal(2, s.SingleNonDuplicate(nums));
+
+            nums = new int[] { 3, 3, 7, 7, 10, 11, 11 };
+            Assert.Equal(10, s.SingleNonDuplicate(nums));
         }
     }
 }
--- LeetCode/UnitTests.cs
+++ LeetCode/UnitTests.cs
@@ -20,19 +20,6 @@ namespace LeetCode
         }
 
         [Fact]
-        public void SingleNonDuplicateTest()
-        {
-            var s = new Solution();
-            int[] nums;
-
-            nums = new int[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
-            Assert.Equal(2, s.SingleNonDuplicate(nums));
-
-            nums = new int[] { 3, 3, 7, 7, 10, 11, 11 };
-            Assert.Equal(10, s.SingleNonDuplicate(nums));
-        }
-
-        [Fact]
         public void RemoveKdigitsTest()
         {
             var s = new Solution();
