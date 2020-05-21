bcc07dd26c6173886866c7ebede726b0190dcc6f
Michael Santos <michael.joseph.santos@gmail.com>
2020-05-21 15:42:04 -0400

    CountSquares accepted

--- LeetCode/Solutions/CountSquares.cs
+++ LeetCode/Solutions/CountSquares.cs
@@ -5,58 +5,58 @@ namespace LeetCode
 {
     public partial class Solution
     {
+        // TODO: Optimize. Beats 19.81% of submissions (https://leetcode.com/submissions/detail/342756876)
         public int CountSquares(int[][] matrix)
         {
             if (matrix == null || matrix.Length == 0)
                 return 0;
 
-            int totalSquares = 0, maxSide;
+            int totalSquares = 0, maxSize;
 
-            // 3rd dimension is the side minus 1 (ex: 0 is 1x1, 1 is 2x2, 2 is 3x3)
-            bool[,,] squaresArray = new bool[matrix.Length, matrix[0].Length, Math.Min(matrix.Length, matrix[0].Length)];
+            // contains the max size for given row, col (ex: squaresArray[1,2] = 3 means biggest square at [1,2] is 3x3)
+            int[,] squaresArray = new int[matrix.Length, matrix[0].Length];
             
-            // TODO: How do we handle jagged arrays?
             for (var row = 0; row < matrix.Length; row++)
                 for (var col = 0; col < matrix[row].Length; col++)
                 {
-                    maxSide = Math.Min(matrix.Length - row, matrix[row].Length - col);
-                    CountSquares_SubarrayCheck(matrix, row, col, maxSide, squaresArray, ref totalSquares);
+                    maxSize = Math.Min(matrix.Length - row, matrix[row].Length - col);
+                    CountSquares_SubarrayCheck(matrix, row, col, maxSize, squaresArray, ref totalSquares);
                 }
 
             return totalSquares;
         }
 
-        private void CountSquares_SubarrayCheck(int[][] matrix, int startRow, int startCol, int maxSide, bool[,,] squaresArray, ref int totalSquares)
+        private void CountSquares_SubarrayCheck(int[][] matrix, int startRow, int startCol, int maxSize, int[,] squaresArray, ref int totalSquares)
         {
             if (matrix[startRow][startCol] != 1)
                 return;
 
-            if (maxSide == 1 )
+            for (var size = 1; size <= maxSize; size++)
             {
-                squaresArray[startRow, startCol, 0] = true;
-                totalSquares++;
-                System.Console.WriteLine($"1x1 square at [{startRow},{startCol}]");
-                return;
-            }
-
-            for (var side = 2; side <= maxSide; side++)
-            {
-                // If a 3x3 array at [0,0] exists, then a 2x2 array at [1,1] exists
-                if (startRow - 1 >= 0 &&
-                    startCol - 1 >= 0 && 
-                    side + 1 <= maxSide &&
-                    squaresArray[startRow - 1, startCol - 1, side + 1])
+                if (size == 1)
                 {
-                    squaresArray[startRow, startCol, side - 1] = true;
-                    System.Console.WriteLine($"{side}x{side} square at [{startRow},{startCol}] based on {side+1}x{side+1} square at [{startRow-1},{startCol-1}]");
+                    squaresArray[startRow, startCol] = size;
                     totalSquares++;
+                    // System.Console.WriteLine($"{totalSquares}: [{startRow},{startCol}] 1x1");
                     continue;
                 }
 
-                // Manually check side x side array at [startRow, startCol]
+                // If a 3x3 array at [0,0] exists, then a 2x2 array at [1,1] exists
+                if (startRow - 1 >= 0 &&
+                    startCol - 1 >= 0 && 
+                    size + 1 <= maxSize &&
+                    squaresArray[startRow - 1, startCol - 1] >= size + 1)
+                {
+                    squaresArray[startRow, startCol] = size;
+                    totalSquares++;
+                    // System.Console.WriteLine($"{totalSquares}: [{startRow},{startCol}] {size}x{size}; [{startRow-1},{startCol-1}] {size+1}x{size+1}");
+                    continue;
+                }
+
+                // Manually check NxN array at [startRow, startCol]
                 var isSquare = true;
-                for (var row = startRow; row < startRow + side; row++)
-                    for (var col = startCol; col < startCol + side; col++)
+                for (var row = startRow; row < startRow + size; row++)
+                    for (var col = startCol; col < startCol + size; col++)
                         if (matrix[row][col] != 1)
                         {
                             isSquare = false;
@@ -65,10 +65,12 @@ namespace LeetCode
 
                 if (isSquare)
                 {
-                    squaresArray[startRow, startCol, side - 1] = true;
-                    System.Console.WriteLine($"{side}x{side}} square at [{startRow},{startCol}] based on manual check");
+                    squaresArray[startRow, startCol] = size;
                     totalSquares++;
+                    // System.Console.WriteLine($"{totalSquares}: [{startRow},{startCol}] {size}x{size} (manual)");
                 }
+                else
+                    break;
             }
         }
     }
@@ -96,6 +98,16 @@ namespace LeetCode
                 new int [] {1,1,0}
             };
             expected = 7; // 6 (1), 1 (2)
+            Assert.Equal(expected, s.CountSquares(matrix));
+            
+            matrix = new int[][] { // test case 21/32
+                new int[] {1,1,0,0,1},
+                new int[] {1,0,1,1,1},
+                new int[] {1,1,1,1,1},
+                new int[] {1,0,1,0,1},
+                new int[] {0,0,1,0,1}
+            };
+            expected = 19; // 17 (1), 2 (2)
             Assert.Equal(expected, s.CountSquares(matrix));
         }
     }
