388a6ee1dd667caa885b3461e0b1f07d34e5e2b5
Michael Santos <michael.joseph.santos@gmail.com>
2020-07-17 12:01:37 -0400

    +DocumentEngine.GetFileProperties()

--- C_8_and_.NET_Core_3_Projects_Using_Azure_2nd_Ed/eBookManager/eBookManager.Engine/DocumentEngine.cs
+++ C_8_and_.NET_Core_3_Projects_Using_Azure_2nd_Ed/eBookManager/eBookManager.Engine/DocumentEngine.cs
@@ -1,7 +1,40 @@
+using System;
+using System.IO;
+
 namespace eBookManager.Engine
 {
     public class DocumentEngine
     {
-        
+        public (string fileName, string fileExtension, DateTime dateCreated, DateTime dateLastAccessed, long fileLength, bool error) GetFileProperties(string filePath)
+        {
+            var fileProperties = (
+                fileName: string.Empty, 
+                fileExtension: string.Empty, 
+                dateCreated: DateTime.MinValue, 
+                dateLastAccessed: DateTime.MinValue,
+                fileLength: 0L,
+                error: false
+            );
+
+            try
+            {
+                var fileInfo = new FileInfo(filePath);
+                fileInfo.Refresh();
+                fileProperties = (
+                    fileInfo.Name,
+                    fileInfo.Extension,
+                    fileInfo.CreationTime,
+                    fileInfo.LastAccessTime,
+                    fileInfo.Length,
+                    false
+                );
+            }
+            catch (Exception)
+            {
+                fileProperties.error = true;
+            }
+
+            return fileProperties;
+        }
     }
 }
